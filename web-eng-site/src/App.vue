1f30eeda5cd9e84b51a19d1d1a5269e7a8517b5f
Dennis1911 <52860328+Dennis1911@users.noreply.github.com>
2022-11-30 18:31:44 +0100

    Basic Website Design

--- web-eng-site/src/App.vue
+++ web-eng-site/src/App.vue
@@ -1,26 +1,117 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Focus</title>
+</head>
+<body>
+  <div id="app">      
+    <div class="container">
+     <div class="menübar">
+      <button  v-on:click="Funktion()" class="dbtn"><img src="./assets/Moon.png">
+        </button> 
+      <a style="float: left; font-size: 35px;" >FOCUS</a>
+      <a>WEATHER</a>
+      <a>SPOTIFY</a>
+      <a class="active" >TIMER</a>      
+    <!--Darkmode button-->
+        </div>
+    </div>
+  </div>
+  <!--Hintergrund Bild-->
+  <div class="bpic">
+  </div>
+</body>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<!--  -->
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+<script>
+export default{
+  name: "app",
+  data (){
+    return {
+      api_key: ""
+    }
+  },
+  methods: {   
+  Funktion() {
+   var element = document.body;
+   element.classList.toggle("dark-mode");
+   
+    }
   }
 }
+  
 </script>
 
+<!--  -->
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  margin: 0;
+  padding: 0;
+	font-family: 'Open Sans Condensed', sans-serif;
+  box-sizing: border-box;
 }
+
+/*container*/
+.menübar{ 
+	overflow: hidden;
+	background-color: #282828;
+}
+
+.menübar a{
+  display: flex;
+  justify-content: space-around;
+  text-decoration: none;
+	float: right;
+	color: #ffffff;
+	align-items: center;
+  min-height: 40px;
+	padding: 35px 20px;
+	font-size: 20px;
+  letter-spacing: 2px;
+}
+
+
+.menübar a:hover {
+	background-color: #4d4b4b;
+}
+
+.menübar a.active {
+  background-color: #4d4b4b;
+  color: rgb(255, 255, 255);
+}
+
+  /*Dark mode, Button und Funktion (Javascript)*/
+  body {
+    background-color: #1d1c1c;
+    color: rgb(226, 219, 219);
+  }
+
+  
+  .dark-mode {
+    background-color: white;
+    color: black;
+  }
+
+  .dbtn{
+    margin-top: 2%;
+    background: #282828;
+    border: none;
+    padding: 10px;
+    float: right;
+  }
+
+  /*Hintergrundbild*/
+  .bpic{
+    
+    background-image: url("./assets/bpic.jpg");
+    min-height:900px;
+    background-position: center;
+    background-size: cover;
+    background-attachment: fixed;
+  }
+
 </style>
